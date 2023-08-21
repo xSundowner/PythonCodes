8334befb4a448aec1cb17ac99b904592f69210e6
Gustavo Oliveira <142353633+xSundowner@users.noreply.github.com>
2023-08-21 16:25:37 -0300

    Update ListaDeCompras.py

--- ListaDeCompras.py
+++ ListaDeCompras.py
@@ -4,12 +4,12 @@ preço = []
 cont = 1
 x = 0
 
-while cont == 1:
+while cont == 1: #Permite a entrada de dados pelo usuário
     p = input("Digite o nome do produto: ")
     q = input("Digite a quantidade: ")
     v = input("Digite o preço: ")
 
-    if len(p)>0 and len(q)>0 and len(p)>0:
+    if len(p)>0 and len(q)>0 and len(p)>0: #Confere se valores não estão vazios e os adiciona a lista
         produtos.append(p)
         qnt.append(q)
         preço.append(v)
@@ -18,7 +18,7 @@ while cont == 1:
     else:
         print("Você precisa digitar todos os valores")
 
-for i in produtos:
+for i in produtos: #Mostra a lista separando-a em categorias
     print("Número / Produto / Quantidade / Preço")
     print(f"{x} - {produtos[x]} / {qnt[x]} / {preço[x]}R$")
     x += 1
